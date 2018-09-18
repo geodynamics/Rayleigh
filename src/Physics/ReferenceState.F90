9b85e2817868cfc88bb0d57007d589cb7b7a525a
Wolfgang Bangerth <bangerth@colostate.edu>
2018-09-18 14:22:55 -0600

    Add a copyright notice to all .F90 files.

--- src/Physics/ReferenceState.F90
+++ src/Physics/ReferenceState.F90
@@ -1,3 +1,23 @@
+!
+!  Copyright (C) 2018 by the authors of the RAYLEIGH code.
+!
+!  This file is part of RAYLEIGH.
+!
+!  RAYLEIGH is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 3, or (at your option)
+!  any later version.
+!
+!  RAYLEIGH is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with RAYLEIGH; see the file LICENSE.  If not see
+!  <http://www.gnu.org/licenses/>.
+!
+
 ! REFERENCE STATE MODULE
 ! Contains routines for initializing the reference state structure
 ! Reference state structure contains all information related to background
