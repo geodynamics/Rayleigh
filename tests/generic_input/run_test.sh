6cf8d62d886a01d709d78a02cd11a61f9b1e808c
Cian Wilson <cwilson@carnegiescience.edu>
2019-07-24 17:15:21 -0600

    Extending the generic_input test to also test magnetism -- Peter Driscoll's work.

--- tests/generic_input/run_test.sh
+++ tests/generic_input/run_test.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
 cd tests/generic_input
+
+# first we run the "base" case, this just sets up and runs Rayleigh for one time-step with hard-coded initial conditions 
+# for the Christensen et al., 2001 benchmark case 1
 cd base
 mpirun -np 4 ../../../bin/rayleigh.dbg
 cd ..
+
+# next we run the same thing but using generic input files to set the initial conditions
 cd script
+# we generate the generic input files two different ways so that both are tested and demonstrated...
+# first we generate the temperature initial condition using rayleigh_spectral_input.py in "script" mode:
 ../../../pre_processing/rayleigh_spectral_input.py -ar 0.35 -sd 1.0 -nt 64 -nr 48 -o bench_t_init \
    -e 'import numpy as np; x = 2*radius - rmin - rmax; rmax*rmin/radius - rmin + 210*0.1*(1 - 3*x*x + 3*(x**4) - x**6)*(np.sin(theta)**4)*np.cos(4*phi)/np.sqrt(17920*np.pi)'
+# then we use a custom python script using rayleigh_spectral_input.py as a module to write the magnetic initial conditions
+PYTHONPATH=../../../pre_processing:$PYTHONPATH python generate_magnetic_input.py
+# finally we run Rayleigh
 mpirun -np 4 ../../../bin/rayleigh.dbg
 cd ..
+
+# after both versions have run, we test the output for errors
 PYTHONPATH=../../post_processing:$PYTHONPATH python test_output.py
 
--- tests/generic_input/test_output.py
+++ tests/generic_input/test_output.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-from rayleigh_diagnostics import Spherical_3D
+from rayleigh_diagnostics import Spherical_3D_multi
 import numpy as np
 import sys
 
-base = Spherical_3D('00000001_0501', path='base/Spherical_3D/')
-script = Spherical_3D('00000001_0501', path='script/Spherical_3D/')
-maxabsdiff = np.abs(base.vals - script.vals).max() 
-print("Maximum difference = {}".format(maxabsdiff,))
+base = Spherical_3D_multi('00000001_0501', path='base/Spherical_3D/')
+script = Spherical_3D_multi('00000001_0501', path='script/Spherical_3D/')
+maxabsdiff = 0.0
+for k in base.vals.keys():
+  maxabsdiffk = np.abs(base.vals[k] - script.vals[k]).max() 
+  print("Maximum difference ({}) = {}".format(k, maxabsdiffk,))
+  maxabsdiff = max(maxabsdiff, maxabsdiffk)
 if maxabsdiff > 1.e-10:
-  print("ERROR: init_type 1 and init_type 8 produced different initial conditions (within a tolerance of 1.e-10)!")
+  print("ERROR: [magnetic_]init_type 1 and [magnetic_]init_type 8 produced different initial conditions (within a tolerance of 1.e-10)!")
   sys.exit(1)
 sys.exit(0)
 
