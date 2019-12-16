961483a3706a6e57eb4bb4e81c9a280078a46a2b
Nick Featherstone <feathern@gmail.com>
2019-12-16 16:32:02 -0700

    re-added print statement

--- src/Physics/Input.F90
+++ src/Physics/Input.F90
@@ -93,11 +93,11 @@ Contains
         !///////////////////
         
         If (my_sim_rank .eq. 0) Write(6,*)'Inside main_input_broadcast!'
-        !If (my_sim_rank .eq. 0) Write(6,*)input_as_string
+        If (my_sim_rank .eq. 0) Write(6,*)input_as_string
         Read(input_as_string, nml=problemsize_namelist)
-        Write(6,*)'problemsize read'
+        !Write(6,*)'problemsize read'
         Read(input_as_string, nml=numerical_controls_namelist)
-        WRite(6,*)'numerical controls read'
+        !WRite(6,*)'numerical controls read'
         Read(input_as_string, nml=physical_controls_namelist)
         Read(input_as_string, nml=temporal_controls_namelist)
         Read(input_as_string, nml=io_controls_namelist)
