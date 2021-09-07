6849ee6b354dd9f462a944cb741435109283bd4a
Loren <loren.matilsky@gmail.com>
2021-09-06 20:20:46 -0700

    may have fixed it

--- src/Physics/PDE_Coefficients.F90
+++ src/Physics/PDE_Coefficients.F90
@@ -755,7 +755,7 @@ Contains
         If (present(filename)) Then
             ref_file = Trim(my_path)//filename
         Else
-            ref_file = 'equation_coefficients'
+            ref_file = Trim(my_path)//'equation_coefficients'
         Endif
 
         If (my_rank .eq. 0) Then
