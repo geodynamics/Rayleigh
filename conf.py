8b5dff96e6a5072aeba3d868d7317a817de025d0
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2022-09-13 11:54:21 -0600

    Add additional theme options and CIG logo.

--- conf.py
+++ conf.py
@@ -56,11 +56,32 @@ nbsphinx_allow_errors = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "collapse_navigation": True,
+    "navigation_depth": 3,
+    "show_toc_level": 3,
+    "repository_url": "https://github.com/geodynamics/rayleigh/",
+    "repository_branch": "master",
+    "path_to_docs":"doc/sphinx/",
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/geodynamics/rayleigh",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "show_navbar_depth": 2,
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "extra_navbar": "<p><img src=\"/en/latest/_static/images/cig_logo_dots.png\" alt=\"CIG Logo\" height=\"80px\"  style=\"padding: 5px;\"/></p>",
+    "home_page_in_toc": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['doc/source']
 
 html_logo = 'doc/source/rayleigh_manual_image_logo.jpeg'
 latex_logo = 'doc/source/rayleigh_manual_image_300dpi.jpeg'
