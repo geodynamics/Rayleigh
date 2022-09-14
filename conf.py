749a48a5b84c719ef65606217ff7de1adf134cd6
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2022-09-13 19:23:07 -0600

    Fix doc path to suggest edits.

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ html_theme_options = {
     "show_toc_level": 3,
     "repository_url": "https://github.com/geodynamics/rayleigh/",
     "repository_branch": "master",
-    "path_to_docs":"doc/sphinx/",
+    "path_to_docs":".",
     "icon_links": [
         {
             "name": "GitHub",
