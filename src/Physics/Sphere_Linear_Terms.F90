e07db6597965779f0cb2bf3493ace8d3ecc8da46
Rafael Fuentes <43425560+jorafb@users.noreply.github.com>
2022-09-14 11:53:42 -0600

    Changed the sign of the buoyancy term associated to Chi. It should be opposite to the sign of temperature

--- src/Physics/Sphere_Linear_Terms.F90
+++ src/Physics/Sphere_Linear_Terms.F90
@@ -217,7 +217,7 @@ Contains
                 amp = -ref%Buoyancy_Coeff
                 Call add_implicit_term(peq, tvar, 0, amp,lp, static = .true.)            ! Gravity    --- Need LHS_Only Flag
 
-                amp = -ref%chi_buoyancy_coeff
+                amp = ref%chi_buoyancy_coeff
                 Call add_implicit_term(peq, chivar, 0, amp,lp, static = .true.)            ! Gravity    --- Need LHS_Only Flag
 
                 amp = ref%dpdr_W_term
@@ -255,7 +255,7 @@ Contains
                 Call add_implicit_term(weq, tvar, 0, amp,lp)            ! Gravity
 
                 ! Chi
-                amp = -ref%chi_buoyancy_coeff/H_Laplacian
+                amp = ref%chi_buoyancy_coeff/H_Laplacian
                 Call add_implicit_term(weq, chivar, 0, amp,lp)          ! Gravity
 
 
