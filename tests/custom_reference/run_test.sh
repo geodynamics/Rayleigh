c4b27a547dea071e1b3786df6ae3cb1f95babf80
Nick Featherstone <feathern@gmail.com>
2022-12-13 18:06:58 -0700

    Changed 'python' command to 'python3'

--- tests/custom_reference/run_test.sh
+++ tests/custom_reference/run_test.sh
@@ -5,7 +5,7 @@ cd tests/custom_reference
 # Run a test where the full reference state is specified
 cd basic
 cp ../../../post_processing/reference_tools.py .
-python gen_case0.py
+python3 gen_case0.py
 
 mpirun -np 4 ../../../bin/rayleigh.dbg
 
