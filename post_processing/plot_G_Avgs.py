ec0b0b385cf717c6bf58f2878450174cd1df093a
Peter Driscoll <pdriscoll@carnegiescience.edu>
2023-06-12 13:52:42 -0600

    something

--- post_processing/plot_G_Avgs.py
+++ post_processing/plot_G_Avgs.py
@@ -159,6 +159,7 @@ ftke = lut[411]  #KE associated with mean theta motion
 fpke = lut[412]  # KE associated with mean azimuthal motion
 
 entropy = lut[501]
+pressure = lut[502]
 
 # To begin with, let's plot the total, mean, and fluctuating kinetic energy density during the initial transient phase, and then during the equilibrated phase.
 
