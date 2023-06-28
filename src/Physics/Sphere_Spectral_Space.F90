01ef32242ecf09f1390f0165655ed757c436d5a1
Rathish Ratnasingam <rathish.ratnsingam@newcastle.ac.uk>
2023-06-28 13:23:12 +0100

    Added indent in line 54 in Sphere_Spectral_Space

--- src/Physics/Sphere_Spectral_Space.F90
+++ src/Physics/Sphere_Spectral_Space.F90
@@ -51,7 +51,7 @@ Contains
                 Write(otstring,t_ofmt)old_deltat
                 Write(tstring,t_ofmt)deltat
                 Call stdout%print(' Timestep has changed from '//Trim(otstring)//' to '//Trim(tstring)//'.')
-        Call stdout%partial_flush()  ! Make SURE that a changing timestep is recorded ...
+                Call stdout%partial_flush()  ! Make SURE that a changing timestep is recorded ...
                          ! ... even at the expense of additional file I/O for redirected stdout
             Endif
             Call StopWatch(seteq_time)%startclock()
