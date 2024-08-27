e90d28d2bf459a02e6383ccce97f921fa1342d03
Nick Featherstone <feathern@gmail.com>
2024-08-27 09:30:36 -0600

    Moved deallocate statement into main Chebyshev "if" block

--- src/Physics/PDE_Coefficients.F90
+++ src/Physics/PDE_Coefficients.F90
@@ -1605,6 +1605,8 @@ Contains
             !Transform back to physical space.
             Call gridcp%From_Spectral(dtemp2,dtemp)
             arr2(:) = dtemp(:,1,1,2)
+            
+            DeAllocate(dtemp,dtemp2)
         
         Else
         
@@ -1615,10 +1617,6 @@ Contains
         ! If desired, convert to logarithmic derivative (default)
         If (.not. present(no_log)) Then
             arr2(:) = arr2(:)/arr1(:)
-        Endif
-
-        If (chebyshev) Then
-           DeAllocate(dtemp,dtemp2)
         Endif
 
     End Subroutine log_deriv
